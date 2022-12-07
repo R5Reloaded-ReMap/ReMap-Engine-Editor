e7bd963a90152a3f8d159f34b282852bdf438311
Julefox_ <Julien.aub13@gmail.com>
2022-12-07 23:52:32 +0100

    add autoDetachDraw

--- Scripts/Custom/DrawNonVerticalZipline.cs
+++ Scripts/Custom/DrawNonVerticalZipline.cs
@@ -10,6 +10,7 @@ public class DrawNonVerticalZipline : MonoBehaviour
     [Header("Settings:")]
     public bool ShowZipline = true;
     public float ShowZiplineDistance = 8000;
+    public bool ShowAutoDetachDistance = true;
 
     [Header("Zipline Parameters:")]
     public float fadeDistance = -1;
@@ -43,6 +44,25 @@ public class DrawNonVerticalZipline : MonoBehaviour
                 var thickness = 3;
             
                 Handles.DrawBezier(startPos, endPos, startPos, endPos, Color.yellow, null, thickness);
+
+                if(ShowAutoDetachDistance)
+                {
+                    // Draw Start Auto Detach
+                    var startDir = zipline_end.position - zipline_start.position;
+                    var startDistance = startDir.magnitude;
+                    var startDirection = startDir / startDistance;
+                    if(autoDetachStart < 0) autoDetachStart = 0;
+
+                    if(autoDetachStart != 0) Handles.DrawBezier(startPos, startPos + startDirection * autoDetachStart, startPos, startPos + startDirection * autoDetachStart, Color.red, null, thickness);
+
+                    // Draw End Auto Detach
+                    var endDir = zipline_start.position - zipline_end.position;
+                    var endDistance = endDir.magnitude;
+                    var endDirection = endDir / endDistance;
+                    if(autoDetachEnd < 0) autoDetachEnd = 0;
+
+                    if(autoDetachEnd != 0) Handles.DrawBezier(endPos, endPos + endDirection * autoDetachEnd, endPos, endPos + endDirection * autoDetachEnd, Color.red, null, thickness);
+                }
             }
         }
     }

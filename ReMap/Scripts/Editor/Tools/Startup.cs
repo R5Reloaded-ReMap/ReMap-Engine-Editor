fc64461db970af7591bd6a630cba03a721af0b80
AyeZeeBB <nxmbx123@hotmail.com>
2023-02-27 21:02:26 -0600

    may have fixed startup issue

--- ReMap/Scripts/Editor/Tools/Startup.cs
+++ ReMap/Scripts/Editor/Tools/Startup.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System;
+using System.Linq;
+using System.Reflection;
 
 [InitializeOnLoad]
 public class Startup : EditorWindow {
 
     static string currentDirectory = Directory.GetCurrentDirectory();
     static string relativeConfigFile = $"Assets/ReMap/Resources/startupconfig.json";
+
+    static bool HasRan = false;
     static Startup()
     {
+        if(HasRan)
+            return;
+
+        HasRan = true;
+
         //These will run when Unity starts up
         TagHelper.CheckAndCreateTags();
 

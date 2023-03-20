1e8204c1edce92329672a38a912e6a8d7df9dfd6
Julefox_ <Julien.aub13@gmail.com>
2023-03-20 02:06:13 +0100

    starting to add DrawZiplineClassData

--- ReMap/Scripts/Editor/Exporting/ImportExportClassesTest.cs
+++ ReMap/Scripts/Editor/Exporting/ImportExportClassesTest.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class JsonData
 {
     public List<PropScriptClassData> Props;
+    public List<DrawZiplineClassData> ZipLines;
 }
 
 [Serializable]
@@ -21,14 +22,6 @@ public class PropScriptClassData
 }
 
 [Serializable]
-public class TransformData
-{
-    public Vector3 position;
-    public Vector3 eulerAngles;
-    public Vector3 localScale;
-}
-
-[Serializable]
 public class PropScriptData
 {
     public bool allowMantle;
@@ -36,4 +29,34 @@ public class PropScriptData
     public int realmID;
     public List<PropScriptParameters> parameters;
     public List<string> customParameters;
+}
+
+[Serializable]
+public class DrawZiplineClassData
+{
+    public Vector3 start_position;
+    public Vector3 end_position;
+    public DrawZiplineSettingsData Settings;
+    public string PathString;
+    public List<PathClass> Path;
+    
+}
+
+[Serializable]
+public class DrawZiplineSettingsData
+{
+    public bool ShowZipline;
+    public float ShowZiplineDistance;
+}
+
+
+/// <summary>
+/// TransformData is used to determine the position / angles of a prefab
+/// </summary>
+[Serializable]
+public class TransformData
+{
+    public Vector3 position;
+    public Vector3 eulerAngles;
+    public Vector3 localScale;
 }

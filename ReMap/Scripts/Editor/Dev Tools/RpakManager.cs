22b7732028ce18302ea3c0ee71e5fc2f0aa7089b
Julefox_ <Julien.aub13@gmail.com>
2023-04-10 02:00:13 +0200

    remove unused using

--- ReMap/Scripts/Editor/Dev Tools/RpakManager.cs
+++ ReMap/Scripts/Editor/Dev Tools/RpakManager.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 

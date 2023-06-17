8f5c0353d598b50819fbf9be056a79deaa99b112
Julefox_ <Julien.aub13@gmail.com>
2023-06-17 16:10:18 +0200

    re do code views struct

--- ReMap/Scripts/Editor/Code Views/CodeViewsWindow.cs
+++ ReMap/Scripts/Editor/Code Views/CodeViewsWindow.cs
@@ -25,21 +25,139 @@ namespace CodeViews
 
     public class CodeViewsWindow : EditorWindow
     {
+        private static WindowStruct windowStruct = new WindowStruct()
+        {
+            MainTab = new[] { "Squirrel Code", "DataTable Code", "Precache Code", "Ent Code", "Other Code" },
+
+            SubTab = new Dictionary< int, string[] >()
+            {
+                { 3, new[] { "script.ent", "snd.ent", "spawn.ent" } },
+                { 4, new[] { "Camera Path" } }
+            },
+
+            SubTabGUI = new Dictionary< Tuple< int, int >, GUIStruct >()
+            {
+                // Squirrel Code
+                    {
+                        WindowStruct.NewTuple( 0, 0 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => ScriptTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = Helper.GetSceneName();
+                                fileInfo = new[] { "Squirrel Code Export", "", $"{functionName}.nut", "nut" };
+                            }
+                        }
+                    },
+
+                // DataTable Code
+                    {
+                        WindowStruct.NewTuple( 1, 0 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => DataTableTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = "remap_datatable";
+                                fileInfo = new[] { "DataTable Code Export", "", $"{functionName}.csv", "csv" };
+                            }
+                        }
+                    },
+
+                // Precache Code
+                    {
+                        WindowStruct.NewTuple( 2, 0 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => PrecacheTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = $"{Helper.GetSceneName()}_Precache";
+                                fileInfo = new[] { "Precache Code Export", "", $"{functionName}.nut", "nut" };
+                            }
+                        }
+                    },
+
+                // Ent Code
+                    // Script Code
+                    {
+                        WindowStruct.NewTuple( 3, 0 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => ScriptEntTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = "mp_rr_remap_script";
+                                fileInfo = new[] { "Ent Code Export", "", $"{functionName}.ent", "ent" };
+                            }
+                        }
+                    },
+                
+                    // Sound Code
+                    {
+                        WindowStruct.NewTuple( 3, 1 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => SoundEntTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = "mp_rr_remap_snd";
+                                fileInfo = new[] { "Ent Code Export", "", $"{functionName}.ent", "ent" };
+                            }
+                        }
+                    },
+                
+                    // Spawn Code
+                    {
+                        WindowStruct.NewTuple( 3, 2 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => SpawnEntTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = "mp_rr_remap_spawn";
+                                fileInfo = new[] { "Ent Code Export", "", $"{functionName}.ent", "ent" };
+                            }
+                        }
+                    },
+
+                // Other Code
+                    // Camera Path Code
+                    {
+                        WindowStruct.NewTuple( 4, 0 ),
+                        new GUIStruct()
+                        {
+                            OnGUI = new FunctionRef[] { () => CameraPathTab.OnGUISettingsTab() },
+
+                            OnStartGUI = () =>
+                            {
+                                functionName = "remap_camera_path";
+                                fileInfo = new[] { "Camera Path Code Export", "", $"{functionName}.nut", "nut" };
+                            }
+                        }
+                    },
+                //
+            },
+
+            RefreshCallback = () =>
+            {
+                Refresh( false );
+            }
+        };
+
         internal static CodeViewsWindow windowInstance;
         internal static string code = "";
         internal static string[] codeSplit = new string[0];
         internal static string functionName = "Unnamed";
-        internal static string[] toolbarTab = new[] { "Squirrel Code", "DataTable Code", "Precache Code", "Ent Code", "Other Code" };
-        internal static string[] toolbarSubTabEntCode = new[] { "script.ent", "snd.ent", "spawn.ent" };
-        internal static string[] toolbarSubTabOtherCode = new[] { "Camera Path" };
         internal static Vector2 scroll;
         internal static Vector2 scrollSettings;
-        internal static int tab = 0;
-        internal static int tab_temp = 0;
-        internal static int tabEnt = 0;
-        internal static int tabEnt_temp = 0;
-        internal static int tabOther = 0;
-        internal static int tabOther_temp = 0;
         internal static int objectTypeInSceneCount = 0;
         internal static int objectTypeInSceneCount_temp = 0;
         internal static Vector3 StartingOffset = Vector3.zero;
@@ -47,6 +165,8 @@ namespace CodeViews
         internal static string additionalCodeHead = "";
         internal static string additionalCodeInBlock = "";
         internal static string additionalCodeBelow = "";
+
+        private static string[] fileInfo = new string[4];
 
         // Menus
         // Show / Hide Settings Menu
@@ -108,14 +228,13 @@ namespace CodeViews
         [ MenuItem( "ReMap/Code Views", false, 25 ) ]
         public static void Init()
         {
-            TagHelper.CheckAndCreateTags(); tab = 0; tabEnt = 0;
+            TagHelper.CheckAndCreateTags();
 
             if ( Helper.IsValid( AdditionalCodeWindow.additionalCode ) ) AdditionalCodeWindow.AdditionalCodeInit();
 
             windowInstance = ( CodeViewsWindow ) GetWindow( typeof( CodeViewsWindow ), false, "Code Views" );
             windowInstance.minSize = new Vector2( 1230, 500 );
             windowInstance.Show();
-            GetFunctionName();
 
             Helper.SetShowStartingOffset( true );
         }
@@ -137,16 +256,6 @@ namespace CodeViews
 
             ShortCut();
 
-            if ( tab != tab_temp || tabEnt != tabEnt_temp || tabOther != tabOther_temp || objectTypeInSceneCount != objectTypeInSceneCount_temp )
-            {
-                GetFunctionName();
-                Refresh( false );
-                tab_temp = tab;
-                tabEnt_temp = tabEnt;
-                tabOther_temp = tabOther;
-                objectTypeInSceneCount_temp = objectTypeInSceneCount;
-            }
-
             GUILayout.BeginVertical( "box" );
                 GUILayout.BeginHorizontal();
 
@@ -155,7 +264,7 @@ namespace CodeViews
                     if ( ShowSettingsMenu )
                     {
                         GUILayout.BeginVertical( "box", GUILayout.Width( 340 ) );
-                            SettingsMenu();
+                            windowStruct.ShowFunc();
                         GUILayout.EndVertical();
                     }
 
@@ -226,36 +335,6 @@ namespace CodeViews
             // Hack: As long as the code generation is not finished, then do not continue the script
             while ( GenerationIsActive ) await Task.Delay( 100 ); // 100 ms
 
-            string[] fileInfo = new string[4];
-
-            switch ( tab )
-            {
-                case 0: // Squirrel Code
-                    fileInfo = new[] { "Squirrel Code Export", "", $"{functionName}.nut", "nut" };
-                    break;
-
-                case 1: // DataTable Code
-                    fileInfo = new[] { "DataTable Code Export", "", $"{functionName}.csv", "csv" };
-                    break;
-
-                case 2: // Precache Code
-                    fileInfo = new[] { "Precache Code Export", "", $"{functionName}.nut", "nut" };
-                    break;
-
-                case 3: // Ent Code
-                    fileInfo = new[] { "Ent Code Export", "", $"{functionName}.ent", "ent" };
-                    break;
-
-                case 4: // Other Code
-                    switch ( tabOther )
-                    {
-                        case 0: // Camera Path Code
-                            fileInfo = new[] { "Camera Path Code Export", "", $"{functionName}.nut", "nut" };
-                        break;
-                    }
-                break;
-            }
-
             var path = EditorUtility.SaveFilePanel( fileInfo[0], fileInfo[1], fileInfo[2], fileInfo[3] );
 
             if ( path.Length == 0 ) return;
@@ -290,24 +369,8 @@ namespace CodeViews
         private static void MainTab()
         {
             GUILayout.BeginVertical( "box" );
-                GUILayout.BeginHorizontal();
-                    tab = GUILayout.Toolbar ( tab, toolbarTab );
-                    if ( GUILayout.Button( new GUIContent( "Refresh", "Refresh Window" ), GUILayout.Width( 100 ) ) ) Refresh();
-                GUILayout.EndHorizontal();
 
-                if ( tab == 3 )
-                {
-                    GUILayout.BeginHorizontal();
-                        tabEnt = GUILayout.Toolbar ( tabEnt, toolbarSubTabEntCode );
-                    GUILayout.EndHorizontal();           
-                }
-
-                if ( tab == 4 )
-                {
-                    GUILayout.BeginHorizontal();
-                        tabOther = GUILayout.Toolbar ( tabOther, toolbarSubTabOtherCode );
-                    GUILayout.EndHorizontal(); 
-                }
+                windowStruct.ShowTab();
 
                 GUILayout.Box( "", GUILayout.ExpandWidth( true ), GUILayout.Height( 2 ) );
 
@@ -324,7 +387,7 @@ namespace CodeViews
                         GUILayout.FlexibleSpace();
 
                         ExportButton();
-                        if ( tab == 0 ) AdditionalCodeButton();
+                        if ( windowStruct.MainTabIdx == 0 ) AdditionalCodeButton();
                         SettingsMenuButton();
                 GUILayout.EndHorizontal();
 
@@ -337,8 +400,6 @@ namespace CodeViews
                     GUILayout.Box( "", GUILayout.ExpandWidth( true ), GUILayout.Height( 2 ) );
 
                     GUILayout.BeginHorizontal();
-                        //WindowUtility.WindowUtility.CreateTextInfo( $"// Page {currentPage + 1} / {maxPage} ( {itemStart} - {end} lines )", "", 400 );
-
                         GUILayout.Label( $" // Page {currentPage + 1} / {maxPage} ( {itemStart} - {end} lines )", EditorStyles.boldLabel, GUILayout.Width( 400 ) );
 
                         WindowUtility.WindowUtility.FlexibleSpace();
@@ -348,65 +409,22 @@ namespace CodeViews
                         WindowUtility.WindowUtility.CreateButton( "Next Page", "", () => { if ( itemEnd < codeSplit.Length ) currentPage++; }, 100 );
                     GUILayout.EndHorizontal();
                 }
+
             GUILayout.EndVertical();
-        }
-
-        private static void SettingsMenu()
-        {
-            switch ( tab )
-            {
-                case 0: // Squirrel Code
-                    ScriptTab.OnGUISettingsTab();
-                    break;
-
-                case 1: // DataTable Code
-                    DataTableTab.OnGUISettingsTab();
-                    break;
-
-                case 2: // Precache Code
-                    PrecacheTab.OnGUISettingsTab();
-                    break;
-
-                case 3: // Ent Code
-                    switch ( tabEnt )
-                    {
-                        case 0: // Script Code
-                            ScriptEntTab.OnGUISettingsTab();
-                            break;
-
-                        case 1: // Sound Code
-                            SoundEntTab.OnGUISettingsTab();
-                            break;
-
-                        case 2: // Spawn Code
-                            SpawnEntTab.OnGUISettingsTab();
-                        break;
-                    }
-                    break;
-
-                case 4: // Other Code
-                    switch ( tabOther )
-                    {
-                        case 0: // Camera Path Code
-                            CameraPathTab.OnGUISettingsTab();
-                        break;
-                    }
-                break;
-            }
         }
 
         private static void ObjectCount()
         {
             string info = "Entity Count";
 
-            switch ( tab )
+            switch ( windowStruct.MainTabIdx )
             {
                 case 2: // Precache Code
                     info = "Models Precached Count";
                     break;
 
                 case 4: // Other Code
-                    switch ( tabOther )
+                    switch ( windowStruct.SubTabIdx )
                     {
                         case 0: // Camera Path Code
                             info = "Paths Count";
@@ -499,7 +517,7 @@ namespace CodeViews
 
             GenerationIsActive = true;
 
-            switch ( tab )
+            switch ( windowStruct.MainTabIdx )
             {
                 case 0: // Squirrel Code
                     code += await ScriptTab.GenerateCode();
@@ -514,7 +532,7 @@ namespace CodeViews
                     break;
 
                 case 3: // Ent Code
-                    switch ( tabEnt )
+                    switch ( windowStruct.SubTabIdx )
                     {
                         case 0: // Script Code
                             code += await ScriptEntTab.GenerateCode();
@@ -530,7 +548,7 @@ namespace CodeViews
                     break;
 
                 case 4: // Other Code
-                    switch ( tabOther )
+                    switch ( windowStruct.SubTabIdx )
                     {
                         case 0: // Camera Path Code
                             code += await CameraPathTab.GenerateCode();
@@ -546,49 +564,6 @@ namespace CodeViews
             CodeViewsMenu.VerifyGenerateObjects();
 
             GenerationIsActive = false;
-        }
-
-        private static void GetFunctionName()
-        {
-            switch ( tab )
-            {
-                case 0: // Squirrel Code
-                    functionName = Helper.GetSceneName();
-                    break;
-
-                case 1: // DataTable Code
-                    functionName = $"remap_datatable";
-                    break;
-
-                case 2: // Precache Code
-                    functionName = $"{Helper.GetSceneName()}_Precache";
-                    break;
-
-                case 3: // Ent Code
-                    switch ( tabEnt )
-                    {
-                        case 0: // Script Code
-                            functionName = $"mp_rr_remap_script";
-                            break;
-
-                        case 1: // Sound Code
-                            functionName = $"mp_rr_remap_snd";
-                            break;
-                        case 2:  // Spawn Code
-                            functionName = $"mp_rr_remap_spawn";
-                        break;
-                    }
-                    break;
-
-                case 4: // Other Code
-                    switch ( tabOther )
-                    {
-                        case 0: // Camera Path Code
-                            functionName = $"remap_camera_path";
-                        break;
-                    }
-                break;
-            }
         }
 
         private static string SetCorrectEntityLabel( int count )
---
+++ ReMap/Scripts/Editor/WindowUtility/WindowStruc.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace WindowUtility
+{
+    public class WindowStruct
+    {
+        public string[] MainTab { get; set; }
+        public Dictionary< int, string[] > SubTab { get; set; }
+        public Dictionary< Tuple< int, int >, GUIStruct > SubTabGUI  { get; set; }
+        public FunctionRef MainTabCallback { get; set; }
+        public FunctionRef SubTabCallback { get; set; }
+        public FunctionRef RefreshCallback { get; set; }
+
+        public int MainTabIdx = 0;
+        public int SubTabIdx = 0;
+        private int MainTabIdxTemp = 0;
+        private int SubTabIdxTemp = 0;
+
+        public void ShowTab()
+        {
+            if ( !Helper.IsValid( MainTab ) || Helper.IsEmpty( MainTab ) ) return;
+
+            GUILayout.BeginHorizontal();
+                MainTabIdx = GUILayout.Toolbar ( MainTabIdx, MainTab );
+                if ( Helper.IsValid( MainTabCallback ) )
+                {
+                    MainTabCallback();
+                }
+            GUILayout.EndHorizontal();
+
+            if ( SubTab.ContainsKey( MainTabIdx ) )
+            {
+                if ( !Helper.IsValid( SubTab[ MainTabIdx ] ) || !Helper.IsEmpty( SubTab[ MainTabIdx ] ) )
+                {
+                    GUILayout.BeginHorizontal();
+                        SubTabIdx = GUILayout.Toolbar ( SubTabIdx, SubTab[ MainTabIdx ] );
+                        if ( Helper.IsValid( SubTabCallback ) )
+                        {
+                            SubTabCallback();
+                        }
+                    GUILayout.EndHorizontal();
+                }
+            }
+
+            if ( MainTabIdx != MainTabIdxTemp )
+            {
+                MainTabIdxTemp = MainTabIdx;
+                SubTabIdx = 0;
+                CommonChanges();
+            }
+
+            if ( SubTabIdx != SubTabIdxTemp )
+            {
+                CommonChanges();
+            }
+        }
+
+        private void CommonChanges()
+        {
+            SubTabIdxTemp = SubTabIdx;
+
+            if ( Helper.IsValid( RefreshCallback ) ) RefreshCallback();
+
+            Tuple< int, int > index = NewTuple( MainTabIdx, SubTabIdx );
+
+            GUIStruct str = SubTabGUI[ index ];
+
+            if ( Helper.IsValid( str ) )
+            {
+                if ( Helper.IsValid( str.OnStartGUI ) ) str.OnStartGUI();
+            }
+        }
+
+        public void ShowFunc( int idx = 0 )
+        {
+            Tuple< int, int > index = NewTuple( MainTabIdx, SubTabIdx );
+
+            GUIStruct str = SubTabGUI[ index ];
+
+            if ( Helper.IsValid( str ) )
+            {
+                if ( Helper.IsValid( str.OnGUI[ idx ] ) ) str.OnGUI[ idx ]();
+            }
+        }
+
+
+        public static Tuple< int, int > NewTuple( int i, int j )
+        {
+            return Tuple.Create( i, j );
+        }
+    }
+
+    public class GUIStruct
+    {
+        public FunctionRef[] OnGUI { get; set; }
+        public FunctionRef OnStartGUI { get; set; }
+    }
+}

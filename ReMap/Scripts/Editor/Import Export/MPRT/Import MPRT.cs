3d580ae883b9e318a93f59cef56d9f4c82591239
AyeZeeBB <nxmbx123@hotmail.com>
2023-07-26 15:23:48 -0500

    Import MPRT Files

---
+++ ReMap/Scripts/Editor/Import Export/MPRT/Import MPRT.cs
@@ -0,0 +1,133 @@
+
+using System;
+using System.Threading;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public class ImportMPRTModels
+{
+    public static string mprtPath = "";
+    public static Vector3 coordinates;
+
+
+
+    [ MenuItem( "ReMap/Import/MPRT", false, 51 ) ]
+    private static async void ImportMPRTCode()
+    {
+        var mprtPath = EditorUtility.OpenFilePanel( "MPRT Import", "", "mprt" );
+
+        byte[] buffer = new byte[4];
+
+            using (BinaryReader reader = new BinaryReader(File.Open(mprtPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                // Read and validate header
+                reader.Read(buffer, 0, 4);
+                int header1 = BitConverter.ToInt32(buffer, 0);
+                if (header1 != 0x7472706D) // check if matches with the expected value
+                {
+                    Debug.LogError("Invalid file.");
+                    return;
+                }
+
+                GameObject objToSpawn = new GameObject("Map Assets");
+                objToSpawn.name = "Map Assets";
+
+                reader.Read(buffer, 0, 4);
+                int header2 = BitConverter.ToInt32(buffer, 0);
+
+                reader.Read(buffer, 0, 4);
+                int numObjects = BitConverter.ToInt32(buffer, 0);
+                
+                // Object lists
+                var scaleList = new List<Vector3>();
+                var posList = new List<Vector3>();
+                var rotList = new List<Vector3>();
+                var nameList = new List<string>();
+
+                var DataList = new List<MPRTData>();
+
+                for (int i = 0; i < numObjects; i++)
+                {
+                    string name = ReadNullTerminatedString(reader);
+                    
+                    float[] posRotScale = new float[7];
+                    for (int j = 0; j < posRotScale.Length; j++)
+                    {
+                        reader.Read(buffer, 0, 4);
+                        posRotScale[j] = BitConverter.ToSingle(buffer, 0);
+                    }
+
+                    MPRTData data = new MPRTData();
+                    data.name = name;
+                    data.pos = new Vector3(posRotScale[1], posRotScale[2], -posRotScale[0]);
+                    data.rot = new Vector3(-posRotScale[4], -posRotScale[5], posRotScale[3]);
+                    data.scale = new Vector3(posRotScale[6], posRotScale[6], posRotScale[6]);
+
+                    //Debug.Log($"X: {posRotScale[3]}, Y: {posRotScale[4]}, Z:{posRotScale[5]}");
+
+                    DataList.Add(data);
+
+                    EditorUtility.DisplayProgressBar( $"Parsing MPRT File: {i}/{numObjects}", $"", ( i + 1 ) / ( float )numObjects );
+
+                    await Helper.Wait();
+                }
+                //EditorUtility.ClearProgressBar();
+                //return;
+
+                // Proceed with the imsplementation of loading the models based on the parsed data...
+
+                for (int i = 0; i < DataList.Count; i++)
+                {
+                    CreateMPRTProp(DataList[i]);
+
+                    EditorUtility.DisplayProgressBar( $"Placing Models: {i}/{DataList.Count}", $"", ( i + 1 ) / ( float )DataList.Count );
+
+                    await Helper.Wait();
+                }
+
+                EditorUtility.ClearProgressBar();
+            }
+    }
+
+    private static void CreateMPRTProp(MPRTData data)
+    {
+        GameObject obj = Helper.CreateGameObject( "", data.name, PathType.Name );
+
+        if ( !Helper.IsValid( obj ) ) return;
+
+        obj.transform.position = data.pos;
+        obj.transform.eulerAngles = data.rot;
+        obj.name = data.name;
+        obj.gameObject.transform.localScale = data.scale;
+
+        GameObject parent = GameObject.Find("Map Assets");
+        if (parent != null)
+            obj.gameObject.transform.parent = parent.transform;
+    }
+
+    private static string ReadNullTerminatedString(BinaryReader reader)
+    {
+        List<byte> byteList = new List<byte>();
+        byte b;
+        while ((b = reader.ReadByte()) != 0)
+        {
+            byteList.Add(b);
+        }
+        return Encoding.UTF8.GetString(byteList.ToArray());
+    }
+}
+
+public class MPRTData
+{
+    public string name;
+    public Vector3 pos;
+    public Vector3 rot;
+    public Vector3 scale;
+}

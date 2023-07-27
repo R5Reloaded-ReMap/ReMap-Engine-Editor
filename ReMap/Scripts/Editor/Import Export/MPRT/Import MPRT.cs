ce0769b79f2b6ad5de112cc1b21bc21883d555c3
AyeZeeBB <nxmbx123@hotmail.com>
2023-07-26 20:23:08 -0500

    Update Import MPRT.cs

--- ReMap/Scripts/Editor/Import Export/MPRT/Import MPRT.cs
+++ ReMap/Scripts/Editor/Import Export/MPRT/Import MPRT.cs
@@ -77,130 +77,101 @@ public class ImportMPRTModels : EditorWindow
         await ImportMPRTCodeAsync();
     }
 
-    void OnFocus() 
-    {
-	    SceneView.duringSceneGui -= this.OnSceneGUI;
-	    SceneView.duringSceneGui += this.OnSceneGUI;
-    }
-
-    void OnDestroy()
-    {
-        SceneView.duringSceneGui -= this.OnSceneGUI;
-    }
-
-    void OnSceneGUI(SceneView sceneView)
-    {
-
-        // Do your drawing here using Handles.
-        if(radius != 0 && source != null)
-        {
-            GameObject obj = source as GameObject;
-            Handles.color = Color.red;
-            Handles.DrawWireDisc(obj.transform.position, Vector3.up, radius, 3.0f );
-        }
-    }
-
     public async Task ImportMPRTCodeAsync()
     {
         byte[] buffer = new byte[4];
 
-            using (BinaryReader reader = new BinaryReader(File.Open(mprtPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+        using (BinaryReader reader = new BinaryReader(File.Open(mprtPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+        {
+            // Read and validate header
+            reader.Read(buffer, 0, 4);
+            int header1 = BitConverter.ToInt32(buffer, 0);
+            if (header1 != 0x7472706D) // check if matches with the expected value
             {
-                // Read and validate header
-                reader.Read(buffer, 0, 4);
-                int header1 = BitConverter.ToInt32(buffer, 0);
-                if (header1 != 0x7472706D) // check if matches with the expected value
-                {
-                    Helper.Ping("Invalid file.");
-                    return;
-                }
+                Helper.Ping("Invalid file.");
+                return;
+            }
 
+            //Create Parent If It Doesnt Exist
+            GameObject parent = GameObject.Find("Map Assets");
+            if (parent == null)
+            {
                 GameObject objToSpawn = new GameObject("Map Assets");
                 objToSpawn.name = "Map Assets";
-
-                reader.Read(buffer, 0, 4);
-                int header2 = BitConverter.ToInt32(buffer, 0);
-
-                reader.Read(buffer, 0, 4);
-                int numObjects = BitConverter.ToInt32(buffer, 0);
-
-                var filterList = filter.Replace(" ", "").Split(',');
-                var DataList = new List<MPRTData>();
-
-                for (int i = 0; i < numObjects; i++)
-                {
-                    string name = ReadNullTerminatedString(reader);
-                    
-                    float[] posRotScale = new float[7];
-                    for (int j = 0; j < posRotScale.Length; j++)
-                    {
-                        reader.Read(buffer, 0, 4);
-                        posRotScale[j] = BitConverter.ToSingle(buffer, 0);
-                    }
-
-                    MPRTData data = new MPRTData();
-                    data.name = name;
-                    data.pos = new Vector3(posRotScale[1], posRotScale[2], -posRotScale[0]);
-                    data.rot = new Vector3(-posRotScale[4], -posRotScale[5], posRotScale[3]);
-                    data.scale = new Vector3(posRotScale[6], posRotScale[6], posRotScale[6]);
-
-                    bool skip = false;
-                    if (!string.IsNullOrEmpty(filter))
-                    {
-                        foreach (var x in filterList)
-                        {
-                            if (name.Contains(x))
-                            {
-                                skip = true;
-                            }
-                        }
-                    }
-                    
-                    if (radius != 0 && source != null)
-                    {
-                        GameObject obj = source as GameObject;
-                        float distance = Vector3.Distance(obj.transform.position, data.pos);
-                        if (distance > radius)
-                        {
-                            skip = true;
-                        }
-                    }
-
-                    if(skip)
-                        continue;
-
-                    DataList.Add(data);
-
-                    EditorUtility.DisplayProgressBar( $"Parsing MPRT File: {i}/{numObjects}", $"", ( i + 1 ) / ( float )numObjects );
-
-                    await Helper.Wait();
-                }
-
-                UnityInfo.SortListByKey( DataList, o => o.name );
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
-
-                for (int i = 0; i < DataList.Count; i++)
-                {
-                    CreateMPRTProp(DataList[i]);
-
-                    double progress = (i + 1) / (double)DataList.Count;
-                    double elapsedTime = stopwatch.Elapsed.TotalSeconds;
-                    double estimatedTotalTime = elapsedTime / progress;
-                    double estimatedRemainingTime = estimatedTotalTime - elapsedTime;
-
-                    int remainingMinutes = (int)(estimatedRemainingTime / 60);
-                    int remainingSeconds = (int)(estimatedRemainingTime % 60);
-
-                    EditorUtility.DisplayProgressBar($"Placing Models: {i}/{DataList.Count}", $"Time remaining: {remainingMinutes:D2}:{remainingSeconds:D2} min", (float)progress);
-
-                    await Helper.Wait();
-                }
-
-                stopwatch.Stop();
-
-                EditorUtility.ClearProgressBar();
             }
+
+            reader.Read(buffer, 0, 4);
+            int header2 = BitConverter.ToInt32(buffer, 0);
+
+            reader.Read(buffer, 0, 4);
+            int numObjects = BitConverter.ToInt32(buffer, 0);
+                
+            string[] filterList = filter.Replace(" ", "").Split(',');
+            var DataList = new List<MPRTData>();
+
+            for (int i = 0; i < numObjects; i++)
+            {
+                //Read Name
+                string name = ReadNullTerminatedString(reader);
+                
+                //Read Position, Rotation, Scale
+                float[] posRotScale = new float[7];
+                for (int j = 0; j < posRotScale.Length; j++)
+                {
+                    reader.Read(buffer, 0, 4);
+                    posRotScale[j] = BitConverter.ToSingle(buffer, 0);
+                }
+
+                //Create Data
+                MPRTData data = new MPRTData();
+                data.name = name;
+                data.pos = new Vector3(posRotScale[1], posRotScale[2], -posRotScale[0]);
+                data.rot = new Vector3(-posRotScale[4], -posRotScale[5], posRotScale[3]);
+                data.scale = new Vector3(posRotScale[6], posRotScale[6], posRotScale[6]);
+
+                //Check Filters And Radius
+                bool filtercheck = FilterCheck(name, filterList);
+                bool radiuscheck = RadiusCheck(data);
+
+                if(filtercheck || radiuscheck)
+                    continue;
+
+                //Add To List
+                DataList.Add(data);
+
+                //Update Progress Bar
+                EditorUtility.DisplayProgressBar( $"Parsing MPRT File: {i}/{numObjects}", $"", ( i + 1 ) / ( float )numObjects );
+
+                await Helper.Wait();
+            }
+
+            //Sort List
+            UnityInfo.SortListByKey( DataList, o => o.name );
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //Create Props
+            for (int i = 0; i < DataList.Count; i++)
+            {
+                CreateMPRTProp(DataList[i]);
+
+                double progress = (i + 1) / (double)DataList.Count;
+                double elapsedTime = stopwatch.Elapsed.TotalSeconds;
+                double estimatedTotalTime = elapsedTime / progress;
+                double estimatedRemainingTime = estimatedTotalTime - elapsedTime;
+
+                int remainingMinutes = (int)(estimatedRemainingTime / 60);
+                int remainingSeconds = (int)(estimatedRemainingTime % 60);
+
+                EditorUtility.DisplayProgressBar($"Placing Models: {i}/{DataList.Count}", $"Time remaining: {remainingMinutes:D2}:{remainingSeconds:D2} min", (float)progress);
+
+                await Helper.Wait();
+            }
+
+            stopwatch.Stop();
+
+            EditorUtility.ClearProgressBar();
+        }
     }
 
     private void CreateMPRTProp(MPRTData data)
@@ -228,6 +199,53 @@ public class ImportMPRTModels : EditorWindow
             byteList.Add(b);
         }
         return Encoding.UTF8.GetString(byteList.ToArray());
+    }
+
+    private bool RadiusCheck(MPRTData data)
+    {
+        if (radius != 0 && source != null)
+        {
+            GameObject obj = source as GameObject;
+            float distance = Vector3.Distance(obj.transform.position, data.pos);
+            if (distance > radius)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool FilterCheck(string name, string[] filterList)
+    {
+        if (!string.IsNullOrEmpty(filter))
+            foreach (var x in filterList)
+                if (name.Contains(x))
+                    return true;
+
+        return false;
+    }
+
+    void OnFocus() 
+    {
+	    SceneView.duringSceneGui -= this.OnSceneGUI;
+	    SceneView.duringSceneGui += this.OnSceneGUI;
+    }
+
+    void OnDestroy()
+    {
+        SceneView.duringSceneGui -= this.OnSceneGUI;
+    }
+
+    void OnSceneGUI(SceneView sceneView)
+    {
+        //Draw Radius If Set
+        if(radius != 0 && source != null)
+        {
+            GameObject obj = source as GameObject;
+            Handles.color = Color.red;
+            Handles.DrawWireDisc(obj.transform.position, Vector3.up, radius, 3.0f );
+        }
     }
 }
 

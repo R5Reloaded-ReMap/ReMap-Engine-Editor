3ea14a6555205cc7caa049b3d762fc81c16ccee1
AyeZeeBB <nxmbx123@hotmail.com>
2023-07-29 17:13:15 -0500

    Revert "."
    This reverts commit a7f8fe8e3a7621fb36a343248a6f026de9598609.

--- ReMap/Scripts/Editor/Dev Tools/FileWrite.cs
+++ ReMap/Scripts/Editor/Dev Tools/FileWrite.cs
@@ -75,4 +75,20 @@ public class ReMapDebug
 
         EditorUtility.ClearProgressBar();
     }
+
+    [MenuItem("Dev Menu/i")]
+    public static void Oups()
+    {
+        foreach( string file in Directory.GetFiles( $"{UnityInfo.currentDirectoryPath}/{UnityInfo.relativePathMaterials}" ) )
+        {
+            if ( file.Contains( ".dds.dds" ) )
+            {
+                string i = Path.GetFileNameWithoutExtension( file );            
+
+                if ( File.Exists( $"{file}" ) && !File.Exists( $"{file.Replace( ".dds.dds", ".dds" )}" ) )File.Move( $"{file}", $"{file.Replace( ".dds.dds", ".dds" )}" );
+
+                if ( File.Exists( $"{file}.meta" ) && !File.Exists( $"{file.Replace( ".dds.dds", ".dds" )}.meta" ) ) File.Move( $"{file}.meta", $"{file.Replace( ".dds.dds", ".dds" )}.meta" );
+            }
+        }
+    }
 }

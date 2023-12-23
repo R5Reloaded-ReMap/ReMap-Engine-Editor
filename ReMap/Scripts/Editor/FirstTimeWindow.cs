b22ccb31de67d7bc72e0fa1bd9dd06e42e0a5f77
AyeZeeBB <nxmbx123@hotmail.com>
2023-12-22 21:42:54 -0600

    more ui stuff

---
+++ ReMap/Scripts/Editor/FirstTimeWindow.cs
@@ -0,0 +1,70 @@
+using UnityEditor;
+using UnityEngine;
+
+public class FirstTimeWindow : EditorWindow
+{
+
+    [MenuItem("ReMap/Startup Window", false, 1080)]
+    public static void OpenWindow()
+    {
+        FirstTimeWindow window = GetWindow<FirstTimeWindow>();
+        window.titleContent = new GUIContent("ReMap Startup");
+        window.minSize = new Vector2(420, 600); // Set minimum width and height for the window
+        window.maxSize = new Vector2(420, 600); // Set maximum width and height for the window
+        window.Show();
+    }
+
+    private Vector2 scrollPosition = Vector2.zero;
+
+    private void OnGUI()
+    {
+        float scrollViewWidth = 420f; // Width of the scroll view
+        GUILayout.BeginArea(new Rect((Screen.width - scrollViewWidth) / 2, 20, scrollViewWidth, Screen.height - 40));
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(scrollViewWidth));
+
+
+        GUILayout.BeginVertical();
+        GUILayout.Label(Resources.Load<Texture2D>("CustomEditor/Welcome") as Texture2D, GUILayout.Width(400), GUILayout.Height(100));
+
+        // Header Label
+        GUIStyle headerStyle = new GUIStyle(GUI.skin.label);
+        headerStyle.fontSize = 18;
+        headerStyle.fontStyle = FontStyle.Bold;
+        GUIStyle subHeaderStyle = new GUIStyle(GUI.skin.label);
+        subHeaderStyle.fontSize = 16;
+        subHeaderStyle.fontStyle = FontStyle.Bold;
+
+        // Description Label
+        GUIStyle descriptionStyle = new GUIStyle(GUI.skin.label);
+        descriptionStyle.wordWrap = true; // Allow wrapping for the description
+        GUILayout.Label("Seamless Prop-Based Design:", headerStyle, GUILayout.Width(400));
+        GUILayout.Label("Remap revolutionizes map creation by exclusively focusing on the power of props. Say goodbye to traditional terrain limitations and hello to a world where every structure, detail, and element is meticulously placed using an extensive library of props. Immerse players in a completely fresh Apex Legends experience with your custom-crafted environments.", descriptionStyle, GUILayout.Width(400));
+        GUILayout.Space(10);
+        GUILayout.Label("Tutorials, Support & Documentation", headerStyle, GUILayout.Width(400));
+        GUILayout.Space(5);
+        GUILayout.Label("Documentation", subHeaderStyle, GUILayout.Width(400));
+        GUILayout.Label("Remap documentation can be found at https://docs.ayezee.app/", descriptionStyle, GUILayout.Width(400));
+        if (GUILayout.Button("Visit Documentation"))
+        {
+            Application.OpenURL("https://docs.ayezee.app/");
+        }
+        GUILayout.Space(5);
+        GUILayout.Label("Support", subHeaderStyle, GUILayout.Width(400));
+        GUILayout.Label("Get access to support on our Discord Server!", descriptionStyle, GUILayout.Width(400));
+        GUILayout.Space(5);
+        if (GUILayout.Button("Join Discord Server"))
+        {
+            Application.OpenURL("https://discord.gg/3f72WZJN6Z");
+        }
+
+        GUILayout.Space(20);
+        GUILayout.Label("First Steps", headerStyle, GUILayout.Width(400));
+        GUILayout.Space(5);
+        GUILayout.Label("We highly recommend you to navigate to 'Levels/Example Level', where you will find a premade example level/", descriptionStyle, GUILayout.Width(400));
+        GUILayout.Label("Feel free to reach out on Discord if you need help!", descriptionStyle, GUILayout.Width(400));
+        GUILayout.EndVertical();
+
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+    }
+}
--- ReMap/Scripts/Editor/Tools/InfoUI.cs
+++ ReMap/Scripts/Editor/Tools/InfoUI.cs
@@ -14,82 +14,84 @@ public class InfoUI : EditorWindow
     private Texture2D m_Logo = null;
     void OnEnable()
     {
-        m_Logo = (Texture2D)Resources.Load("Images/logo",typeof(Texture2D));
+        m_Logo = (Texture2D)Resources.Load("Images/logo", typeof(Texture2D));
     }
 
     void OnGUI()
     {
         GUILayout.BeginHorizontal();
 
-            GUILayout.BeginVertical("box", GUILayout.Height(150), GUILayout.Width(150));
-                GUILayout.Label(m_Logo, GUILayout.Height(140), GUILayout.Width(140));
-            GUILayout.EndVertical();
+        GUILayout.BeginVertical("box", GUILayout.Height(150), GUILayout.Width(150));
+        GUILayout.Label(m_Logo, GUILayout.Height(140), GUILayout.Width(140));
+        GUILayout.EndVertical();
 
-            GUILayout.BeginVertical("box", GUILayout.Height(150));
-                //Docs
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("ReMap Docs:", EditorStyles.boldLabel, GUILayout.Width(120));
-                    if (GUILayout.Button("remap.ayezee.app"))
-                        Application.OpenURL("https://remap.ayezee.app/");
-                GUILayout.EndHorizontal();
-                //Github
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("Github:", EditorStyles.boldLabel, GUILayout.Width(120));
-                    if (GUILayout.Button("github.com/R5Reloaded-ReMap"))
-                        Application.OpenURL("https://github.com/R5Reloaded-ReMap/");
-                GUILayout.EndHorizontal();
-                //Discord
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("Discord:", EditorStyles.boldLabel, GUILayout.Width(120));
-                    if (GUILayout.Button("discord.gg/snsyDVa2fn"))
-                        Application.OpenURL("https://discord.gg/snsyDVa2fn");
-                GUILayout.EndHorizontal();
-                //Twitter
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("Twitter:", EditorStyles.boldLabel, GUILayout.Width(120));
-                    if (GUILayout.Button("twitter.com/ReMapR5R"))
-                        Application.OpenURL("https://twitter.com/ReMapR5R");
-                GUILayout.EndHorizontal();
-                //Base Scripts
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("Base Scripts:", EditorStyles.boldLabel, GUILayout.Width(120));
-                    if (GUILayout.Button("github.com/R5Reloaded-ReMap/scripts_r5"))
-                        Application.OpenURL("hhttps://github.com/R5Reloaded-ReMap/scripts_r5");
-                GUILayout.EndHorizontal();
-                //Flowstate Scripts
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("Flowstate Scripts:", EditorStyles.boldLabel, GUILayout.Width(120));
-                    if (GUILayout.Button("github.com/ColombianGuy/r5_flowstate"))
-                        Application.OpenURL("https://github.com/ColombianGuy/r5_flowstate");
-                GUILayout.EndHorizontal();
-            GUILayout.EndVertical();
+        GUILayout.BeginVertical("box", GUILayout.Height(150));
+        GUILayout.Label("Links:", EditorStyles.boldLabel, GUILayout.Width(120));
+        GUILayout.Space(20);
+        //Docs
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("ReMap Docs", EditorStyles.boldLabel, GUILayout.Width(120));
+        if (GUILayout.Button("docs.ayezee.app"))
+            Application.OpenURL("https://docs.ayezee.app/");
+        GUILayout.EndHorizontal();
+        //Github
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Github", EditorStyles.boldLabel, GUILayout.Width(120));
+        if (GUILayout.Button("github.com/R5Reloaded-ReMap"))
+            Application.OpenURL("https://github.com/R5Reloaded-ReMap/");
+        GUILayout.EndHorizontal();
+        //Discord
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Discord", EditorStyles.boldLabel, GUILayout.Width(120));
+        if (GUILayout.Button("discord.gg/snsyDVa2fn"))
+            Application.OpenURL("https://discord.gg/snsyDVa2fn");
+        GUILayout.EndHorizontal();
+        //Twitter
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Twitter", EditorStyles.boldLabel, GUILayout.Width(120));
+        if (GUILayout.Button("twitter.com/ReMapR5R"))
+            Application.OpenURL("https://twitter.com/ReMapR5R");
+        GUILayout.EndHorizontal();
+        //Base Scripts
+        //GUILayout.BeginHorizontal();
+        //    GUILayout.Label("Base Scripts:", EditorStyles.boldLabel, GUILayout.Width(120));
+        //    if (GUILayout.Button("github.com/R5Reloaded-ReMap/scripts_r5"))
+        //        Application.OpenURL("hhttps://github.com/R5Reloaded-ReMap/scripts_r5");
+        //GUILayout.EndHorizontal();
+        //Flowstate Scripts
+        //GUILayout.BeginHorizontal();
+        //    GUILayout.Label("Flowstate Scripts:", EditorStyles.boldLabel, GUILayout.Width(120));
+        //    if (GUILayout.Button("github.com/ColombianGuy/r5_flowstate"))
+        //        Application.OpenURL("https://github.com/ColombianGuy/r5_flowstate");
+        //GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
         GUILayout.EndHorizontal();
 
         GUI.contentColor = Color.white;
         GUILayout.BeginHorizontal();
-            //Credits
-            GUILayout.BeginVertical("box", GUILayout.Height(120));
-                GUILayout.Label("Credits:", EditorStyles.boldLabel);
-                GUILayout.Label("Unity Scripts/Export Code/Custom Prefabs:", EditorStyles.boldLabel);
-                GUILayout.Label("  AyeZee#6969 \n");
-                GUILayout.Label("Unity Scripts/Export Code/Importing Models:", EditorStyles.boldLabel);
-                GUILayout.Label("  Julefox#0050");
-            GUILayout.EndVertical();
-            //Donate
-            GUILayout.BeginVertical("box", GUILayout.Height(120));
-                GUILayout.Label("Donate:", EditorStyles.boldLabel);
-                GUILayout.Label("If you would like to donate to show support\nfor the project. :)");
-                GUILayout.Label("");
-                if (GUILayout.Button("paypal.me/AyeZeeBB"))
-                    Application.OpenURL("https://www.paypal.com/paypalme/AyeZeeBB");
-                if (GUILayout.Button("paypal.me/Julefx"))
-                    Application.OpenURL("https://www.paypal.com/paypalme/Julefx");
-            GUILayout.EndVertical();
+        //Credits
+        GUILayout.BeginVertical("box", GUILayout.Height(120));
+        GUILayout.Label("Credits:", EditorStyles.boldLabel);
+        GUILayout.Label("Unity Scripts/Export Code/Custom Prefabs:", EditorStyles.boldLabel);
+        GUILayout.Label("  AyeZee#6969 \n");
+        GUILayout.Label("Unity Scripts/Export Code/Importing Models:", EditorStyles.boldLabel);
+        GUILayout.Label("  Julefox#0050");
+        GUILayout.EndVertical();
+        //Donate
+        GUILayout.BeginVertical("box", GUILayout.Height(120));
+        GUILayout.Label("Donate:", EditorStyles.boldLabel);
+        GUILayout.Label("If you would like to donate to show support\nfor the project. :)");
+        GUILayout.Label("");
+        if (GUILayout.Button("paypal.me/AyeZeeBB"))
+            Application.OpenURL("https://www.paypal.com/paypalme/AyeZeeBB");
+        if (GUILayout.Button("paypal.me/Julefx"))
+            Application.OpenURL("https://www.paypal.com/paypalme/Julefx");
+        GUILayout.EndVertical();
         GUILayout.EndHorizontal();
 
         //Version
         GUILayout.BeginVertical("box");
-            GUILayout.Label($"{UnityInfo.ReMapVersion}");
+        GUILayout.Label($"{UnityInfo.ReMapVersion}");
         GUILayout.EndVertical();
     }
 }

f54cb032a1e058513d76bcffea90840a24016968
AyeZeeBB <nxmbx123@hotmail.com>
2023-12-23 22:07:44 -0600

    Added apex info into the default transform component

---
+++ ReMap/Scripts/Objects/CustomEditors/TransformEditor.cs
@@ -0,0 +1,168 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Transform), true), CanEditMultipleObjects]
+public class TransformEditor : Editor
+{
+    GameObject SelectedObject = null;
+    private static readonly Type transformInspectorType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.TransformInspector");
+    private UnityEditor.Editor transformEditor = null;
+
+    void OnEnable()
+    {
+        transformEditor = CreateEditor(serializedObject.targetObjects, transformInspectorType);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (Selection.activeTransform)
+            SelectedObject = Selection.activeTransform.gameObject;
+        else
+            SelectedObject = null;
+
+        EditorGUILayout.LabelField(" Unity Transforms:", CustomEditorStyle.LabelStyle);
+        EditorGUILayout.Space(5);
+        transformEditor.OnInspectorGUI();
+
+        EditorGUILayout.Space(20);
+
+        EditorGUILayout.LabelField(" Apex Info:", CustomEditorStyle.LabelStyle);
+        EditorGUILayout.Space(5);
+
+        if (SelectedObject)
+        {
+            string[] splitArray = SelectedObject.name.Split(char.Parse(" "));
+            string model = "$\"" + splitArray[0].Replace("#", "/") + ".rmdl" + "\"";
+            if (!splitArray[0].Contains("mdl#"))
+                model = splitArray[0];
+
+            //Asset
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Asset:");
+            GUILayout.TextField(model, GUILayout.Width(200));
+            if (GUILayout.Button("Copy", GUILayout.Width(60)))
+                GUIUtility.systemCopyBuffer = model;
+            GUILayout.EndHorizontal();
+
+
+            //Origin
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Origin:");
+            GUILayout.TextField(BuildOrigin(SelectedObject), GUILayout.Width(200));
+            if (GUILayout.Button("Copy", GUILayout.Width(60)))
+                GUIUtility.systemCopyBuffer = BuildOrigin(SelectedObject);
+            GUILayout.EndHorizontal();
+
+            //Angles
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Angle:");
+            GUILayout.TextField(BuildAngles(SelectedObject), GUILayout.Width(200));
+            if (GUILayout.Button("Copy", GUILayout.Width(60)))
+                GUIUtility.systemCopyBuffer = BuildAngles(SelectedObject);
+            GUILayout.EndHorizontal();
+
+            float w = 0;
+            float d = 0;
+            float h = 0;
+
+            foreach (Renderer o in SelectedObject.GetComponentsInChildren<Renderer>())
+            {
+                if (o.bounds.size.z > w)
+                    w = o.bounds.size.z;
+
+                if (o.bounds.size.x > d)
+                    d = o.bounds.size.x;
+
+                if (o.bounds.size.y > h)
+                    h = o.bounds.size.y;
+            }
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Size (Unity Vector):");
+            GUILayout.TextField("X: " + d.ToString().Replace(",", ".") + "   Y: " + h.ToString().Replace(",", ".") + "   Z: " + w.ToString().Replace(",", "."), GUILayout.Width(267));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Size (Apex Vector):");
+            GUILayout.TextField("X: " + w.ToString().Replace(",", ".") + "   Y: " + d.ToString().Replace(",", ".") + "   Z: " + h.ToString().Replace(",", "."), GUILayout.Width(267));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Scale:");
+            GUILayout.TextField(SelectedObject.transform.localScale.x.ToString().Replace(",", "."), GUILayout.Width(267));
+            GUILayout.EndHorizontal();
+        }
+        else
+        {
+            //Asset
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Asset:");
+            GUILayout.TextField("$\"\"", GUILayout.Width(200));
+            if (GUILayout.Button("Copy", GUILayout.Width(60)))
+                GUIUtility.systemCopyBuffer = "";
+            GUILayout.EndHorizontal();
+
+            //Origin
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Origin:");
+            GUILayout.TextField("<0,0,0>", GUILayout.Width(200));
+            if (GUILayout.Button("Copy", GUILayout.Width(60)))
+                GUIUtility.systemCopyBuffer = "<0,0,0>";
+            GUILayout.EndHorizontal();
+
+            //Angles
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Angle:");
+            GUILayout.TextField("<0,0,0>", GUILayout.Width(200));
+            if (GUILayout.Button("Copy", GUILayout.Width(60)))
+                GUIUtility.systemCopyBuffer = "<0,0,0>";
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Size (Unity Vector):");
+            GUILayout.TextField("W: 0   D: 0   H: 0", GUILayout.Width(267));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Size (Apex Vector):");
+            GUILayout.TextField("W: 0   D: 0   H: 0", GUILayout.Width(267));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Scale:");
+            GUILayout.TextField("0", GUILayout.Width(267));
+            GUILayout.EndHorizontal();
+        }
+    }
+
+    public static string BuildOrigin(GameObject go)
+    {
+        Vector3 vec = go.transform.position;
+        float x = -vec.z;
+        float y = vec.x;
+        float z = vec.y;
+
+        return $"< {x}, {y}, {z} >";
+    }
+
+    public static string BuildAngles(GameObject go)
+    {
+        Vector3 vec = go.transform.rotation.eulerAngles;
+
+        float x = -WrapAngle(vec.x);
+        float y = -WrapAngle(vec.y);
+        float z = WrapAngle(vec.z);
+
+        return $"< {x}, {y}, {z} >";
+    }
+
+    public static float WrapAngle(float angle)
+    {
+        angle %= 360;
+
+        if (angle > 180) return angle - 360;
+
+        return angle;
+    }
+}
